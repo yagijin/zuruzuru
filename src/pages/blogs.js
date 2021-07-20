b55b75acb171ee6fa6c29f555f6a4bca6c281222
yagijin <jinjin.0327.jinjin@gmail.com>
2021-07-21 01:19:31 +0900

    単純な検索の追加

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -1,30 +1,77 @@
 import * as React from "react";
 import { Link, graphql, useStaticQuery } from "gatsby";
 
-const AboutPage = () => {
-  const recentBlogs = useStaticQuery(graphql`
-    query RecentBlogsQuery {
-      allMdx(sort: { fields: frontmatter___date, order: DESC }, limit: 3) {
+const BlogsPage = () => {
+  const [blogs, setBlogs] = React.useState([]);
+  const [dispBlogs, setDispBlogs] = React.useState([]);
+  const [textbox, setTextbox] = React.useState("");
+
+  const allBlogs = useStaticQuery(graphql`
+    query AllBlogsQuery {
+      allMdx(sort: { fields: frontmatter___date, order: DESC }) {
         nodes {
           frontmatter {
             date(formatString: "MMMM D, YYYY")
             title
             description
             url
+            tags
           }
         }
       }
     }
   `);
 
-  const card = recentBlogs.allMdx.nodes.map((node) => {
+  React.useEffect(() => {
+    setBlogs(allBlogs.allMdx.nodes);
+    setDispBlogs(allBlogs.allMdx.nodes);
+  }, []);
+
+  const filterBlogs = (text) => {
+    setDispBlogs(
+      blogs.filter(
+        (blog) =>
+          blog.frontmatter.title.toLowerCase().includes(text.toLowerCase()) ||
+          blog.frontmatter.description
+            .toLowerCase()
+            .includes(text.toLowerCase()) ||
+          blog.frontmatter.tags.some((tag) => {
+            return tag.toLowerCase().includes(text.toLowerCase());
+          })
+      )
+    );
+  };
+
+  const filterTags = (text) => {
+    setDispBlogs(
+      blogs.filter((blog) => {
+        return blog.frontmatter.tags.some((tag) => {
+          return tag.toLowerCase().includes(text.toLowerCase());
+        });
+      })
+    );
+  };
+
+  const handleChange = (event) => {
+    setTextbox(event.target.value);
+    filterBlogs(event.target.value);
+  };
+
+  const card = dispBlogs.map((blog) => {
     return (
-      <div key={node.frontmatter.title} className="card">
+      <div key={blog.frontmatter.title} className="card">
         <div className="card-header">
-          <Link to={node.frontmatter.url}>{node.frontmatter.title}</Link>
+          <Link to={blog.frontmatter.url}>{blog.frontmatter.title}</Link>
+          {blog.frontmatter.tags.map((tag) => {
+            return (
+              <span key={tag} onClick={() => filterTags(tag)}>
+                {tag}
+              </span>
+            );
+          })}
         </div>
         <div className="card-body">
-          <p>{node.frontmatter.description}</p>
+          <p>{blog.frontmatter.description}</p>
         </div>
       </div>
     );
@@ -32,12 +79,15 @@ const AboutPage = () => {
 
   return (
     <main>
-      <h1>Recent Blogs</h1>
-      <title>About Page</title>
+      <title>Search Page</title>
+      <h1>Blogs</h1>
+      <label>
+        検索
+        <input type="text" size="10" value={textbox} onChange={handleChange} />
+      </label>
       {card}
-      <Link to="/">Home</Link>
     </main>
   );
 };
 
-export default AboutPage;
+export default BlogsPage;
