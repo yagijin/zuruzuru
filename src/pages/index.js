1d895385976fbb470f8a50dc532ffd2ffc9b0436
yagijin <jinjin.0327.jinjin@gmail.com>
2021-07-24 00:05:32 +0900

    雑多なテンプレート

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import * as React from "react";
+import { Link } from "gatsby";
 
 const IndexPage = () => {
   return (
     <main>
       <title>Home</title>
       <h1>This is Home Page</h1>
+      <Link to="/recent">テックブログをススル</Link>
+      <Link to="/map/morning">モーニングマップ🗺</Link>
+      <Link to="/zurustagram">ズルスタグラム📷</Link>
     </main>
   );
 };
---
+++ src/pages/map/morning.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+const MorningMap = () => {
+  return (
+    <main>
+      <title>Morning Map</title>
+      <h1>Morning</h1>
+    </main>
+  );
+};
+
+export default MorningMap;
