1801c5bf490c299e4cd23f33975391d64eff4acb
yagijin <jinjin.0327.jinjin@gmail.com>
2021-07-28 23:32:11 +0900

    ブログのテンプレートの作成

---
+++ tools/blog-initializer.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Println("タイトルは？")
+	scanner.Scan()
+	title := scanner.Text()
+
+	fmt.Println("概要は？")
+	scanner.Scan()
+	description := scanner.Text()
+
+	date := time.Now().Format(time.RFC3339)
+	filename := strings.Split(date, "T")[0] + "-" + title
+
+	file, err := os.Create("./src/blogs/" + filename + ".md")
+	if err != nil {
+		panic(err)
+	}
+	text := "---\ntitle: " + title + "\ndate: " + date + "\ndescription: " + description + "\nurl: " + "/blog/" + filename + "/" + "\ntags:\n\t-\n---	\n"
+	file.Write([]byte(text))
+	file.Close()
+}
