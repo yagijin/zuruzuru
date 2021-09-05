cc57ff17c369ca83350de3c5b281ef3ed012363a
yagijin <jinjin.0327.jinjin@gmail.com>
2021-09-05 21:15:34 +0900

    update files

--- tools/ogp/ogp_maker.sh
+++ tools/ogp/ogp_maker.sh
@@ -9,7 +9,7 @@ convert \
 -font "/System/Library/Fonts/ヒラギノ角ゴシック W8.ttc" -fill "#121212" -pointsize 46 -gravity center \
 -size 1200x630 -background none caption:"$title" mask.png
 
-composite -compose over ./mask.png ./template.png ../../static/ogp/ogp_"$1".png
+composite -compose over ./mask.png ./tools/ogp/template.png ./static/ogp/ogp_"$1".png
 
 rm ./mask.png
 
--- tools/ogp/update_ogp.sh
+++ tools/ogp/update_ogp.sh
@@ -2,20 +2,20 @@
 echo "## start"
 echo "## removing old ogps..."
 
-rm -rf ../../static/ogp/
+rm -rf ./static/ogp/
 
-mkdir -p ../../static/ogp/
+mkdir -p ./static/ogp/
 
 echo "## removed"
 
-filenames="$(ls ../../src/blogs)"
+filenames="$(ls ./src/blogs)"
 
 echo "## processing..."
 
 echo "$filenames" | while read line
 do
   # substr(string, start, length) では、startからlength文字を取り出す
-  sh ogp_maker.sh "$(echo $line | awk '{print substr($0, 12, (length($0)-15))}')"
+  sh ./tools/ogp/ogp_maker.sh "$(echo $line | awk '{print substr($0, 12, (length($0)-15))}')"
 done
 
 echo "## done, all blog's ogps are updated"
