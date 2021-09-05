1fcbf5db14cf6fa49107e4c7c9f13e7bf22fd545
yagijin <jinjin.0327.jinjin@gmail.com>
2021-09-05 22:16:23 +0900

    update iroiro

--- tools/ogp/update_ogp.sh
+++ tools/ogp/update_ogp.sh
@@ -1,21 +1,17 @@
 
 echo "## start"
-echo "## removing old ogps..."
-
-rm -rf ./static/ogp/
-
-mkdir -p ./static/ogp/
-
-echo "## removed"
 
 filenames="$(ls ./src/blogs)"
+ogps="$(ls ./static/ogp)"
 
 echo "## processing..."
 
 echo "$filenames" | while read line
 do
-  # substr(string, start, length) では、startからlength文字を取り出す
-  sh ./tools/ogp/ogp_maker.sh "$(echo $line | awk '{print substr($0, 12, (length($0)-15))}')"
+  if [ ! -e "ogp_${line}.png" ];then
+    # substr(string, start, length) では、startからlength文字を取り出す
+    sh ./tools/ogp/ogp_maker.sh "$(echo $line | awk '{print substr($0, 12, (length($0)-15))}')"  
+  fi
 done
 
-echo "## done, all blog's ogps are updated"
+echo "## done"
