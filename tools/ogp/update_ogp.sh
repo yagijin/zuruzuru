794b0a5df9119044059056f3d71ab02bb0fe3c95
yagijin <jinjin.0327.jinjin@gmail.com>
2021-09-05 22:24:12 +0900

    update tool

--- tools/ogp/update_ogp.sh
+++ tools/ogp/update_ogp.sh
@@ -8,7 +8,7 @@ echo "## processing..."
 
 echo "$filenames" | while read line
 do
-  if [ ! -e "ogp_${line}.png" ];then
+  if [ ! -e "./static/ogp/ogp_$(echo $line | awk '{print substr($0, 12, (length($0)-15))}').png" ];then
     # substr(string, start, length) では、startからlength文字を取り出す
     sh ./tools/ogp/ogp_maker.sh "$(echo $line | awk '{print substr($0, 12, (length($0)-15))}')"  
   fi
