68f2c5ee856a7fce8712da0d42d519e393ae6b9b
yagijin <jinjin.0327.jinjin@gmail.com>
2022-03-08 23:18:03 +0900

    remove map系を削除

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,6 +3,3 @@ import "./src/styles/global.scss";
 /* prism js */
 import "prismjs/themes/prism-okaidia.css";
 import "prismjs/plugins/line-numbers/prism-line-numbers.css";
-
-/* leaflet */
-import "leaflet/dist/leaflet.css";
