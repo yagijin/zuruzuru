ca46c4318a40984c6d04f67952cee93f31e9718a
yagijin <jinjin.0327.jinjin@gmail.com>
2022-03-10 01:03:53 +0900

    fix formatできるようになった

--- src/components/Reading.tsx
+++ src/components/Reading.tsx
@@ -1,12 +1,11 @@
-/* default packages */
-import * as React from "react";
-import { graphql, useStaticQuery } from "gatsby";
+import * as React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
 
 /* components */
-import Title from "../components/Title";
+import Title from '../components/Title'
 
 const Reading = () => {
-  const allBlogs = useStaticQuery(graphql`
+  const latestBook = useStaticQuery(graphql`
     query ReadingQuery {
       allMdx(
         sort: { fields: frontmatter___date, order: DESC }
@@ -24,20 +23,20 @@ const Reading = () => {
         }
       }
     }
-  `);
+  `)
 
   return (
     <div>
       <Title title="Reading" emoji="📖" />
-      {allBlogs.allMdx.nodes[0] && (
+      {latestBook.allMdx.nodes[0] && (
         <>
-          <a href={allBlogs.allMdx.nodes[0].frontmatter.url}>
-            {allBlogs.allMdx.nodes[0].frontmatter.title}
+          <a href={latestBook.allMdx.nodes[0].frontmatter.url}>
+            {latestBook.allMdx.nodes[0].frontmatter.title}
           </a>
         </>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default Reading;
+export default Reading
