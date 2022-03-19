21712692c292bae7489564bbbb4b0f52cb1c7d06
yagijin <jinjin.0327.jinjin@gmail.com>
2022-03-19 18:01:15 +0900

    fix googlefontの扱いを変更

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,28 +1,17 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
-import Helmet from 'react-helmet'
+import GoogleFonts from '../GoogleFonts'
 import * as styles from './Footer.module.scss'
 
 const Footer = () => {
   return (
     <>
+      <GoogleFonts />
       <footer className={styles.footer}>
         <hr />
         <Link to="/privacy">Privacy Policy</Link>
         <p>© 2019-{new Date().getFullYear()} yagijin, All Rights Reserved.</p>
       </footer>
-      <Helmet>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin=""
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700;900&display=swap"
-          rel="stylesheet"
-        />
-      </Helmet>
     </>
   )
 }
--- src/components/GoogleFonts/GoogleFonts.tsx
+++ src/components/GoogleFonts/GoogleFonts.tsx
@@ -1,16 +1,22 @@
 /* default packages */
-import * as React from "react";
-import { Helmet } from "react-helmet";
+import * as React from 'react'
+import { Helmet } from 'react-helmet'
 
 const GoogleFonts = () => {
   return (
     <Helmet>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
       <link
         rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Noto+Sans+JP&effect=anaglyph|emboss|fire|fire-animation|neon|outline|shadow-multiple|3d|3d-float|"
       />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Roboto+Slab&family=Noto+Sans+JP:wght@400;700;900&display=swap"
+      />
     </Helmet>
-  );
-};
+  )
+}
 
-export default GoogleFonts;
+export default GoogleFonts
