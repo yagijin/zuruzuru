ef5f0f46fdc2782e2b111f051dfbd75ccb85d3cf
yagijin <yagijin_dev@ymail.ne.jp>
2022-07-17 21:49:14 +0900

    fix mdとmdxが混ざったせいでファイル名の長さが変わりogpの生成にバグが発生していた

--- tools/ogp/update.sh
+++ tools/ogp/update.sh
@@ -1,19 +1,21 @@
 
+# 開始
 echo "## start: update ogp"
 
 # blogのmarkdownファイルのファイル名を変数に入れる
 filenames="$(ls ./src/markdowns/blogs)"
-ogps="$(ls ./static/ogp)"
-
-echo "## processing..."
 
 echo "$filenames" | while read line
 do
+  # 先頭11文字の日付と後部の拡張子を除く
+  basename="$(echo ${line:11} | xargs basename -s .mdx | xargs basename -s .md)"
+
   # すでにogp画像が存在する場合はなにもしない
-  # substr(string, start, length) で、startからlength文字を取り出す
-  if [ ! -e "./static/ogp/ogp_$(echo $line | awk '{print substr($0, 12, (length($0)-15))}').png" ];then
-    sh ./tools/ogp/generate.sh "$(echo $line | awk '{print substr($0, 12, (length($0)-15))}')"  
+  # 存在しない場合、genarate.shで作成する
+  if [ ! -e "./static/ogp/ogp_$basename.png" ];then
+    sh ./tools/ogp/generate.sh $basename  
   fi
 done
 
+# 終了
 echo "## done : updated all"
