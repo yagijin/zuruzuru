86e6af33516c83d8ca94c66bd237b2e4ea371776
yagijin <yagijin_dev@ymail.ne.jp>
2022-07-18 01:12:19 +0900

    update topページでpageLayoutを使うようにする

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,35 @@
 /* default packages */
 import * as React from 'react'
-import * as styles from '../styles/pages/index.module.scss'
+import * as styles from '@/styles/pages/index.module.scss'
 
 /* components */
+import PageLayout from '@/layouts/PageLayout'
 import RecentBlogs from '../components/RecentBlogs'
 import Profile from '../components/Profile'
 import Marquee from '../components/Marquee'
-import Footer from '../components/Footer'
 import Reading from '../components/Reading'
-import Head from '../components/Head'
 import OtherContents from '../components/OtherContents'
 import Todo from '../components/Todo'
 
 const IndexPage = () => {
   return (
-    <>
-      <Head
-        info={{
-          title: 'Top',
-          type: 'website',
-          description: 'yagijinのサイト',
-          url: '',
-        }}
-      />
-      <div className="page-wrapper-60">
-        <main className={styles.index}>
-          <Profile />
-          <Reading />
-          <RecentBlogs />
-          <Todo />
-          <OtherContents />
-          <Marquee />
-        </main>
-        <Footer />
-      </div>
-    </>
+    <PageLayout
+      head={{
+        title: 'Top',
+        type: 'website',
+        description: 'yagijinのサイト',
+        url: '',
+      }}
+    >
+      <main className={styles.index}>
+        <Profile />
+        <Reading />
+        <RecentBlogs />
+        <Todo />
+        <OtherContents />
+        <Marquee />
+      </main>
+    </PageLayout>
   )
 }
 
