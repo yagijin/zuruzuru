fb0fdf92d2a0a9bf9e00bf544bd6a6bc44eae09d
yagijin <yagijin_dev@ymail.ne.jp>
2022-07-18 00:26:23 +0900

    update 404ページでpageLayoutを使うようにする

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,29 +1,21 @@
 import * as React from 'react'
-import * as styles from '../styles/pages/404.module.scss'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
-import Head from '../components/Head'
+import PageLayout from '@/layouts/PageLayout'
 
 const NotFoundPage = () => {
   return (
-    <>
-      <Head
-        info={{
-          title: '404',
-          type: 'website',
-          description: '404 page of zuruzuru',
-          url: '/404',
-        }}
-      />
-      <div className="page-wrapper">
-        <Header title="Not found" link="./" />
-        <main className={styles.notfound}>
-          <h1>Page Not Found</h1>
-          <p>お探しのページは見つかりませんでした。</p>
-        </main>
-        <Footer />
-      </div>
-    </>
+    <PageLayout
+      head={{
+        title: '404',
+        type: 'website',
+        description: '404 page of zuruzurura.men',
+        url: '/404',
+      }}
+    >
+      <>
+        <h1>Page Not Found</h1>
+        <p>お探しのページは見つかりませんでした。</p>
+      </>
+    </PageLayout>
   )
 }
 
